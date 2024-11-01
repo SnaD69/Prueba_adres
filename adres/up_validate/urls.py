be5bd5ad75cdecb597d06343f128c33627d02243
SnaD69 <andersom.asprilla@gmail.com>
2024-11-01 00:48:42 -0500

    README add

--- adres/up_validate/urls.py
+++ adres/up_validate/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from up_validate import views
+from up_validate import views , validar_txt
+ 
 
 
 urlpatterns = [
     path('validar_csv/', views.validar_csv, name='validar_csv'),
+    path('validar_txt/', validar_txt, name='validar_txt'),
 ]
